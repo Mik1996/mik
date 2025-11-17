11834260e2485085cb82331296c288fbab31c903
michele.messina@gellify.com <michelemessina96@gmail.com>
2025-11-17 20:34:01 +0100

    fix(theme): :bug: Styling background color in html tag too

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -21,14 +21,14 @@ export class ThemeService {
   constructor() {
     effect(() => {
       const theme = this.selectedTheme();
-      this.document.body.classList.add(theme);
+      this.document.documentElement.classList.add(theme);
       localStorage.setItem(LocalStorageKey.THEME, theme);
       console.log('Selected theme:', theme);
     });
 
     effect(() => {
       const mode = this.selectedMode();
-      this.document.body.classList.add(mode);
+      this.document.documentElement.classList.add(mode);
       localStorage.setItem(LocalStorageKey.MODE, mode);
       console.log('Selected mode:', mode);
     });
@@ -36,13 +36,13 @@ export class ThemeService {
 
   setTheme = (themeName: ThemeNameType) => {
     if (this.selectedTheme() === themeName) return;
-    this.document.body.classList.remove(themeName);
+    this.document.documentElement.classList.remove(themeName);
     this.selectedTheme.set(themeName);
   };
 
   setMode = (modeName: ModeNameType) => {
     if (this.selectedMode() === modeName) return;
-    this.document.body.classList.remove(modeName);
+    this.document.documentElement.classList.remove(modeName);
     this.selectedMode.set(modeName);
   };
 }
